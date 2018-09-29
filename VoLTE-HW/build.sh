5343df2d39a975eaf08e88f3544bc723e025ba9a
penn5 <penn5@users.noreply.github.com>
2018-09-29 13:26:49 +0100

    Remove a ton of unnecessary stuff and go back to patching smali
    Going back to smali because it's unavoidable! Also correctly convert to compat (including imsservice and manifest) and remove huawei process definitions from the manifest.

--- VoLTE-HW/build.sh
+++ VoLTE-HW/build.sh
@@ -11,16 +11,16 @@ system_folder="$1"
 vendor_folder="$2"
 libdst="HwIms/lib/arm64-v8a/"
 
+# Decompile
 rm -Rf HwIms
 java -jar ../apktool.jar d "$system_folder"/app/HwIms/HwIms.apk
-if [ ! -d HwIms/smali ];then
-	(
-	cd HwIms
-	../../vdexExtractor -i "$system_folder"/app/HwIms/oat/arm64/HwIms.vdex -o .
-	java -jar ../../baksmali.jar d HwIms_classes.dex
-	mv out smali
-	)
-fi
+
+cd HwIms
+
+# Devdex classes.vdex into classes.dex
+../../vdexExtractor -i "$system_folder"/app/HwIms/oat/arm64/HwIms.vdex -o .
+
+# Copy libs
 mkdir -p "$libdst"
 find "$system_folder"/app/HwIms/lib/arm64 -type f -exec cp '{}' "$libdst" \; 
 find "$system_folder"/app/HwIms/lib/arm64 -type l -exec readlink '{}' + | \
@@ -29,54 +29,33 @@ find "$system_folder"/app/HwIms/lib/arm64 -type l -exec readlink '{}' + | \
 		cp "$system_folder"/$p "$libdst"
 	done
 
-cp "$system_folder"/lib64/libnativeloader.so "$libdst"
-cp "$system_folder"/lib64/libnativehelper.so "$libdst"
-cp "$system_folder"/lib64/libcutils.so "$libdst"
-cp "$system_folder"/lib64/libutils.so "$libdst"
-cp "$system_folder"/lib64/libgui.so "$libdst"
-cp "$system_folder"/lib64/libbinder.so "$libdst"
-cp "$system_folder"/lib64/libc++.so "$libdst"
-cp "$system_folder"/lib64/libbacktrace.so "$libdst"
-cp "$system_folder"/lib64/libvndksupport.so "$libdst"
-cp "$system_folder"/lib64/libui.so "$libdst"
-cp "$system_folder"/lib64/libhidlbase.so "$libdst"
-cp "$system_folder"/lib64/libhidltransport.so "$libdst"
-cp "$system_folder"/lib64/libbase.so "$libdst"
-cp "$system_folder"/lib64/libunwind.so "$libdst"
-cp "$system_folder"/lib64/libhardware.so "$libdst"
-cp "$system_folder"/lib64/libhwbinder.so "$libdst"
-cp "$system_folder"/lib64/liblzma.so "$libdst"
-
-for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore-utils.so;do
-	newName="$(echo "$i" |sed -E -e 's/^andr/libA/g' -e 's/@/-/g')"
-	cp "$system_folder"/lib64/$i "$libdst"/$newName
-done
-
-for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore-utils.so;do
-	newName="$(echo "$i" |sed -E -e 's/^andr/libA/g' -e 's/@/-/g')"
-	sed -i -E "s/$i/$newName/g" "$libdst"/*.so
-done
-
-(
-set -x
-rm -Rf build
-mkdir build
-find src -name \*.java |while read f;do
-base="$(dirname "$f" |sed -E 's;(\./)?src/;;g')"
-out="build/$base"
-mkdir -p "$out"
-javac -cp ../libs/framework.jar:../libs/ims-common.jar -d "$out" "$f" 
-done
-$ANDROID_HOME/build-tools/28.0.2/d8 --output build/ --lib ../libs/framework.jar --lib ../libs/ims-common.jar $(find build -name \*.class)
-java -jar ../baksmali.jar d build/classes.dex -o HwIms/smali
-)
-
-sed -i -E \
-	-e 's;Landroid/telephony/ims/feature/MMTelFeature;Landroid/telephony/ims/compat/feature/MMTelFeature;g' \
-	-e 's;Landroid/telephony/ims/stub/ImsUtListenerImplBase;Landroid/telephony/ims/compat/stub/ImsUtListenerImplBase;g' \
-	$(find -name \*.smali)
+# Repack with the libs and classes, remove old version and unpack again
 java -jar ../apktool.jar b HwIms
-LD_LIBRARY_PATH=../signapk/ java -jar ../signapk/signapk.jar -a 4096\
+cp HwIms/dist/HwIms.apk HwImsTmp.apk
+rm -Rf HwIms
+java -jar ../apktool.jar d HwImsTmp.apk
+
+# Patch smali!
+sed -i -E \
+	-e 's;Landroid/telephony/ims/feature/;Landroid/telephony/ims/compat/feature/;g' \
+	-e 's;Landroid/telephony/ims/stub/;Landroid/telephony/ims/compat/stub/;g' \
+	-e 's;Landroid/telephony/ims/ImsService;Landroid/telephony/ims/compat/ImsService;g' \
+	-e 's;Lcom/android/ims/HwImsManager;Lcom/android/ims/ImsManager;g' \
+	$(find -name \*.smali)
+
+# Patch Manifest
+sed -i \
+	-e 's; android:process="com.huawei.telephony.ims";;g' \
+	-e 's;android.telephony.ims.ImsService;android.telephony.ims.compat.ImsService;g' \
+	-e 's;<uses-permission android:name="com.huawei.permission.USE_LEGACY_INTERFACE"/>;;g' 
+	-e 's;<uses-permission android:name="com.huawei.permission.NET_RAW"/>;;g' \
+	-e 's;<uses-permission android:name="com.android.huawei.permission.mt.missing.tips"/>;;g' \
+	-e 's;<uses-permission android:name="com.hisi.permission.VOWIFI_SPECIAL"/>;;g' \
+	HwIms/AndroidManifest.xml
+
+# Repack and sign!
+java -jar ../apktool.jar b HwIms
+LD_LIBRARY_PATH=../signapk/ java -jar ../signapk/signapk.jar \
 	../keys/platform.x509.pem \
 	../keys/platform.pk8 \
 	HwIms/dist/HwIms.apk HwIms.apk
