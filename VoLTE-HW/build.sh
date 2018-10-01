60add3a55f00ad438f5377a561c2d478c913e661
penn5 <penn5@users.noreply.github.com>
2018-10-01 16:37:27 +0100

    Update build.sh
    Fixup stuff and delete useless copypastas. Also update SDK

--- VoLTE-HW/build.sh
+++ VoLTE-HW/build.sh
@@ -3,66 +3,37 @@
 set -e
 
 if [ "$#" -ne 2 ];then
-	echo "Usage: $0 /path/to/system/ /path/to/vendor/"
+	echo "Usage: $0 /path/to/HwIms/folder"
 	exit 1
 fi
 
-system_folder="$1"
-vendor_folder="$2"
+app_folder="$1"
 libdst="HwIms/lib/arm64-v8a/"
 
 rm -Rf HwIms
-java -jar ../apktool.jar d "$system_folder"/app/HwIms/HwIms.apk
+java -jar ../apktool.jar d "$app_folder"/HwIms.apk
 if [ ! -d HwIms/smali ];then
 	(
 	cd HwIms
-	../../vdexExtractor -i "$system_folder"/app/HwIms/oat/arm64/HwIms.vdex -o .
+	../../vdexExtractor -i "$app_folder"/oat/arm64/HwIms.vdex -o .
 	java -jar ../../baksmali.jar d HwIms_classes.dex
 	mv out smali
 	)
 fi
 mkdir -p "$libdst"
-find "$system_folder"/app/HwIms/lib/arm64 -type f -exec cp '{}' "$libdst" \; 
-find "$system_folder"/app/HwIms/lib/arm64 -type l -exec readlink '{}' + | \
+find "$app_folder"/lib/arm64 -type f -exec cp '{}' "$libdst" \; 
+find "$app_folder"/lib/arm64 -type l -exec readlink '{}' + | \
 	while read f;do
 		p="$(echo $f |sed -E 's;/system;;g')"
 		cp "$system_folder"/$p "$libdst"
 	done
-
-cp "$system_folder"/lib64/libnativeloader.so "$libdst"
-cp "$system_folder"/lib64/libnativehelper.so "$libdst"
-cp "$system_folder"/lib64/libcutils.so "$libdst"
-cp "$system_folder"/lib64/libutils.so "$libdst"
-cp "$system_folder"/lib64/libgui.so "$libdst"
-cp "$system_folder"/lib64/libbinder.so "$libdst"
-cp "$system_folder"/lib64/libc++.so "$libdst"
-cp "$system_folder"/lib64/libbacktrace.so "$libdst"
-cp "$system_folder"/lib64/libvndksupport.so "$libdst"
-cp "$system_folder"/lib64/libui.so "$libdst"
-cp "$system_folder"/lib64/libhidlbase.so "$libdst"
-cp "$system_folder"/lib64/libhidltransport.so "$libdst"
-cp "$system_folder"/lib64/libbase.so "$libdst"
-cp "$system_folder"/lib64/libunwind.so "$libdst"
-cp "$system_folder"/lib64/libhardware.so "$libdst"
-cp "$system_folder"/lib64/libhwbinder.so "$libdst"
-cp "$system_folder"/lib64/liblzma.so "$libdst"
-
-for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore-utils.so;do
-	newName="$(echo "$i" |sed -E -e 's/^andr/libA/g' -e 's/@/-/g')"
-	cp "$system_folder"/lib64/$i "$libdst"/$newName
-done
-
-for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore-utils.so;do
-	newName="$(echo "$i" |sed -E -e 's/^andr/libA/g' -e 's/@/-/g')"
-	sed -i -E "s/$i/$newName/g" "$libdst"/*.so
-done
 
 (
 set -x
 rm -Rf build
 mkdir build
 javac -cp ../libs/framework.jar:../libs/telephony-common.jar:../libs/ims-common.jar -d build $(find src -name \*.java)
-$ANDROID_HOME/build-tools/28.0.2/d8 --output build/ --lib ../libs/framework.jar --lib ../libs/telephony-common.jar --lib ../libs/ims-common.jar $(find build -name \*.class)
+$ANDROID_HOME/build-tools/28.0.3/d8 --output build/ --lib ../libs/framework.jar --lib ../libs/telephony-common.jar --lib ../libs/ims-common.jar $(find build -name \*.class)
 java -jar ../baksmali.jar d build/classes.dex -o HwIms/smali
 )
 
