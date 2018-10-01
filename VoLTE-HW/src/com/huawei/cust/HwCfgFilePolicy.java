9cbe403e17d837360c66518638ef0302330d5958
penn5 <penn5@users.noreply.github.com>
2018-10-01 12:02:20 +0100

    No longer assume that /noexist doesn't exist
    This is ok, because a non-existent file will cause the same exception to be raised in the smali anyway...

---
+++ VoLTE-HW/src/com/huawei/cust/HwCfgFilePolicy.java
@@ -0,0 +1,10 @@
+package com.huawei.cust;
+
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class HwCfgFilePolicy {
+	public static File getCfgFile(String path, int a) {
+		throw new FileNotFoundException("STUBBY STUB STUB");
+	}
+}
